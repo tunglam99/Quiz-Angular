d6757bf53db224c57a22036da3e88cb23012587a
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-03-02 15:01:57 +0700

    create model user

---
+++ src/app/model/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  email: string;
+  phoneNumber:string;
+  role:
+}
